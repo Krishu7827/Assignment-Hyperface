712976311469a316a6f865740a022b15f4caa649
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2024-02-01 09:56:57 +0530

    xyz

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Scroll.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+
+const  ScrollBar = ({header,text,data,ind})=>{
+ 
+    const Notify = ()=>{
+        toast('Wait, it is going to start',{
+            position: "top-right",
+autoClose: 3000,
+ 
+closeOnClick: true,
+
+type:'info'
+        })
+    }
+
+
+    return(
+        <div className='Scroll-Header'>
+            <div style={{width:'30px'}}>
+            <ToastContainer />
+            </div>
+            
+            <p style={{color:"white",fontSize:'24px',fontWeight:500}} >{header}</p>
+            <p style={{color:"#B6BBC4",fontSize:'14px'}}>{text}</p>
+        <section className="container">
+     {data.map((el,ind)=>
+     <div key={ind}>
+        <div onClick={Notify} className='card' style={{background:`linear-gradient(to left, ${el.color}, #FFD6A5)`}}>
+
+        </div>
+        <p style={{color:"white",fontSize:'17px',fontWeight:'500'}}>{el.title}</p>
+        <p style={{color:"#B6BBC4",fontSize:'14px'}}>{`${el.chapters} Chapters ${el.duration}`}</p>
+        </div>
+     )}
+      </section>
+      </div>
+    )
+}
+
+export default ScrollBar
